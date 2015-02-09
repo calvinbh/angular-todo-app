a4cc54d8b6ee75d246eb573f98281976bcc93cee
Calvin Williams <calvinbh@gmail.com>
2015-02-09 13:23:57 -0700

    initial commit

---
+++ angularTodoapp/app/HomeCtrl.js
@@ -0,0 +1,21 @@
+﻿app.controller('HomeCtrl', function HomeCtrl($scope) {
+    $scope.todos = [{ val: "Meeting tomorrow @ 2pm", completed: false },
+                    { val: "Plan Vaction", completed: false },
+                    { val: "Study Node.js", completed: false }, ];
+
+    $scope.todo = function (val, completed) {
+        this.val = val;
+        this.completed = false;    
+    };
+
+    $scope.addNewTodo = function () {
+        var todo = new $scope.todo($scope.todo.val, $scope.todo.completed);
+        $scope.todos.unshift(todo);
+        $scope.newTodo = '';
+    };
+
+    $scope.removeTodo = function (index) {
+        $scope.todos.splice(index, 1);
+    };
+    
+});
